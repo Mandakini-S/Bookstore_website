71fe3cd30c9038c6d90571fdbddd939c9a803b3d
Mandakini Sapkota <mandakinisapkota@gmail.com>
2024-07-07 21:54:26 +0545

    Resolve issue related to registration

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -3,10 +3,14 @@ from .models import UserData
 
 
 class UserSerializer(serializers.ModelSerializer):
-
+    email = serializers.EmailField(required=True)
+    name = serializers.CharField(required=True)
+    password = serializers.CharField(min_length=8, write_only=True)
+    
     class Meta:
         model = UserData
         fields = ["id", "email", "name", "password"]
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = UserData.objects.create(email=validated_data['email'],
@@ -15,5 +19,4 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
-    
     
--- backend/account/views.py
+++ backend/account/views.py
@@ -2,12 +2,34 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
-
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
 
 # view for registering users
 class RegisterView(APIView):
-    def post(self, request):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format='json'):
         serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json)
+        return Response(serializer.errors)
+    
+
+class BlacklistTokenUpdateView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = ()
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
